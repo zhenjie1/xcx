0bea4e6723569734f0a444374e1839bc9675561a
yema <1304119519@qq.com>
2024-03-05 21:26:49 +0800

    path

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,7 @@ import * as login from './login'
 // create an axios instance
 export const axiosIns = axios.create({
   adapter: uniAdapter,
-  baseURL: 'https://pay.pyzjhhdq.comapi/', // url = base url + request url
+  baseURL: 'https://pay.pyzjhhdq.com/api/', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000, // request timeout
 })
